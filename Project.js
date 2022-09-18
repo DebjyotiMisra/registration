9f716f19d92005a38f71e98f0aca85f9e94ac6c5
DEBJYOTI MISRA <101012189+DebjyotiMisra@users.noreply.github.com>
2022-09-18 20:25:48 +0530

    Add files via upload

---
+++ Project.js
@@ -0,0 +1,72 @@
+function registration()
+	{
+
+		var name= document.getElementById("t1").value;
+		var email= document.getElementById("t2").value;
+		var uname= document.getElementById("t3").value;
+		var pwd= document.getElementById("t4").value;			var cpwd= document.getElementById("t5").value;
+		
+        //email id expression code
+		var pwd_expression =/^(?=.*[A-Z])/
+		var letters = /^[A-Za-z]+$/;
+		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+if(email=='')
+		{
+			alert('Please enter your user email id');
+		}
+		else if (!filter.test(email))
+		{
+			alert('Email should be "example@gmail.com" like that.');
+		}
+		else if(uname=='')
+		{
+			alert('Please enter the user name.');
+		}
+		else if(!letters.test(uname))
+		{
+			alert('User name field required only alphabet characters');
+		}
+		else if(pwd=='')
+		{
+			alert('Please enter Password');
+		}
+		else if(cpwd=='')
+		{
+			alert('Enter Confirm Password');
+		}
+		else if(!pwd_expression.text(pwd))
+		{
+		   alert('Enter correct password Formate');	
+       
+		}
+		else if(pwd != cpwd)
+		{
+			alert ('Password not Matched');
+		}
+
+
+
+
+
+		else if(document.ementById("t5").value.length < 6)
+		{
+			alert ('Password minimum length is 6');
+		}
+		else if(document.getElementById("t5").value.length > 12)
+		{
+			alert ('Password max length is 12');
+		}
+		else
+		{				                            
+               alert('Thank You for Login');
+		}
+	}
+	function clearFunc()
+	{
+		document.getElementById("t1").value="";
+		document.getElementById("t2").value="";
+		document.getElementById("t3").value="";
+		document.getElementById("t4").value="";
+		document.getElementById("t5").value="";
+	}
